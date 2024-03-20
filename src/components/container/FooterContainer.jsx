c2c98d34e066618f56e97eb6c6e9a19d5760b60c
Your Name <you@example.com>
2024-03-20 10:22:24 +0300

    modify FooterContainer

--- src/components/container/FooterContainer.jsx
+++ src/components/container/FooterContainer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {FaFacebookSquare, FaInstagram,FaTwitter, FaLinkedin} from 'react-icons/fa';
+//facebook
+//instagramm
+//twitter
+//linkedln
 const FooterContainer = () => {
   return (
-    <div class="container">FooterContainer</div>
+    <div class="px-4 mx-auto max-w-[1152px]">
+	  <div class='flex flex-row justify-center items-center'>
+	  	<FaFacebookSquare className='mr-[35px]'/>
+	  	<FaInstagram className='mr-[35px]'/>
+		<FaTwitter className='mr-[35px]'/>
+	  	<FaLinkedin />
+	  </div>
+	  <p class='text-center text-sm mt-[26px]'>Copyright @2020 All rights reserved </p>
+  </div>
   )
 }
 
